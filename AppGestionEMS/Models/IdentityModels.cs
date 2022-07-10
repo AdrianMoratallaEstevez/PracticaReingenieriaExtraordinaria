3b8ad6727cf3948a5fe851479de8339561654519
adrianMoratallaEstevez <adrian.moratalla.estevez@alumnos.upm.es>
2022-07-10 11:52:25 +0200

    Crear clase y controlador de Matriculas #8 #9

--- AppGestionEMS/Models/IdentityModels.cs
+++ AppGestionEMS/Models/IdentityModels.cs
@@ -48,5 +48,7 @@ namespace AppGestionEMS.Models
         public System.Data.Entity.DbSet<AppGestionEMS.Models.Grupo> Grupoes { get; set; }
 
         public System.Data.Entity.DbSet<AppGestionEMS.Models.AsignacionDocente> AsignacionDocentes { get; set; }
+
+        public System.Data.Entity.DbSet<AppGestionEMS.Models.Matriculas> Matriculas { get; set; }
     }
 }
---
+++ AppGestionEMS/Models/Matriculas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEMS.Models
+{
+    public class Matriculas
+    {
+        [Key]
+        [Column(Order = 1)]
+        public string AlumnoId { get; set; } // Clave Foranea
+        public virtual ApplicationUser Alumno { get; set; } //Navigation Property
+        [Key]
+        [Column(Order = 2)]
+        public int CursoId { get; set; } // Clave Foranea
+        public virtual Cursos Curso { get; set; } //Navigation Property
+        [Key]
+        [Column(Order = 3)]
+        public int GrupoId { get; set; } // Clave Foranea
+        public virtual Grupo Grupo { get; set; } //Navigation Property
+    }
+}
